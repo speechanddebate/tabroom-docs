dc248fd4b17e3fd57b9b0d4ae154f114e8415cd3
Aaron Hardy <ashtarcommunications@gmail.com>
2025-08-15 08:42:23 -0600

    Back to base path

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -18,7 +18,7 @@ const config: Config = {
   url: 'https://docs.tabroom.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/tabroom-docs/',
+  baseUrl: '/',
   trailingSlash: false,
 
   // GitHub pages deployment config.
